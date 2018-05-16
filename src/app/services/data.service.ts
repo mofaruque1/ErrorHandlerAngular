975aca42f695b288e51023ca5258dc0cebd74243
Omor Faruque <mdomor.faruque@rci.rogers.ca>
2018-05-16 11:57:13 -0400

    global error handler v--

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,12 +5,23 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class DataService {
 
-  data:any;
+  data: any;
   constructor(private http: HttpClient) { }
 
-  getData(postId:number) {
-  
-    return this.http.get('https://jsonplaceholder.typicode.com/posts/'+postId);
+  getData(postId: number) {
+    return this.http.get('https://jsonplaceholder.typicode.com/posts/' + postId);
+  }
+
+  authenticateUser(username: string, password: string) {
+    return this.http.post('https://dpt-gatewaydev11.canadaeast.cloudapp.azure.com/user/login', JSON.stringify(
+      {
+        "username": username,
+        "password": password,
+        "realm": "ROGERS",
+        "type": "EXTERNAL",
+        "language": "en"
+      }
+    ));
   }
 
 }
